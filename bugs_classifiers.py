90de6b7931bc063da538db7319a18a073232e7b7
julia-bindi <edduarddo057@gmail.com>
2025-01-25 14:41:52 -0300

    update project with the code final version

--- bugs_classifiers.py
+++ bugs_classifiers.py
@@ -3,30 +3,36 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 
 from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+from sklearn.model_selection import cross_val_score
 
 import pandas as pd
 import random
+import optuna
 
-def split_dataset(dataset_path: str, features: list, target: str) -> list:
+def split_dataset(dataset_path: str, target: str) -> list:
     df = pd.read_csv(dataset_path)
 
-    df_features = df[features]
+    all_features = df.columns.values.tolist()
+    all_features.remove(target)
+    df_features = df[all_features]
     df_target = df[target]
 
-    df_features_train, df_feature_test, df_target_train, df_target_test = train_test_split(df_features, df_target, test_size=0.25, random_state=random.random()*100)
+    df_features_train, df_feature_test, df_target_train, df_target_test = train_test_split(df_features, df_target, test_size=0.25, random_state=int(random.random()*100))
 
     return [df_features, df_features_train, df_feature_test, df_target, df_target_train, df_target_test]
 
 def get_metrics(target, predicted) -> dict:
     return {
         "accuracy" : accuracy_score(target, predicted),
-        "f1-score" : f1_score(target, predicted),
-        "recall" : recall_score(target, predicted),
-        "precision" : precision_score(target, predicted)
+        "f1-score" : f1_score(target, predicted, average='weighted'),
+        "recall" : recall_score(target, predicted, average='weighted'),
+        "precision" : precision_score(target, predicted, average='weighted')
     }
 
 def print_metrics(metrics: dict) -> None:
@@ -62,53 +68,143 @@ def print_comparative_metrics(metrics: dict) -> None:
         print(f'-'*(len_methods+((len_metrics+5)*len(metrics_keys))+6), end='')
     print()
 
+def random_forest_classification(dfs) -> dict: 
+    def objective(trial):
+        
+        n_estimators = trial.suggest_int('n_estimators', 100, 1000)
+        max_depth = trial.suggest_int('max_depth', 10, 100)
+        min_samples_split = trial.suggest_int('min_samples_split', 2, 10)
+        min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 5)
+        
+        model = RandomForestClassifier(
+            n_estimators=n_estimators, 
+            max_depth=max_depth, 
+            min_samples_leaf=min_samples_leaf, 
+            min_samples_split=min_samples_split, 
+            max_features='log2',
+            random_state=int(random.random()*100)
+        )
 
-def random_forest_classification(dataset_path: str, features: list, target: str) -> dict: 
-    dfs = split_dataset(dataset_path, features, target)
+        score = cross_val_score(model, dfs[0], dfs[3], n_jobs=-1, cv=3).mean()
+        
+        return score
 
-    classifier = RandomForestClassifier(n_estimators=200, random_state=random.random()*100)
+    study = optuna.create_study(direction='maximize', study_name='Random_Forest')
+    study.optimize(objective, n_trials=100)
+    best_params = study.best_trial.params
 
-    classifier.fit(dfs[1], dfs[4])
-    target_predicted = classifier.predict(dfs[2])
+    best_model = RandomForestClassifier(**best_params)
+    best_model.fit(dfs[1], dfs[4])
 
-    return get_metrics(dfs[5, target_predicted])
+    target_predicted = best_model.predict(dfs[2])
 
-def decision_tree_classification(dataset_path: str, features: list, target: str) -> dict:
-    dfs = split_dataset(dataset_path, features, target)
+    return get_metrics(dfs[5], target_predicted)
 
-    classifier = DecisionTreeClassifier(random_state=random.random()*100)
+def decision_tree_classification(dfs) -> dict:
+    def objective(trial):
+        
+        max_depth = trial.suggest_int('max_depth', 10, 100)
+        min_samples_split = trial.suggest_int('min_samples_split', 2, 10)
+        min_samples_leaf = trial.suggest_int('min_samples_leaf', 1, 5)
 
-    classifier.fit(dfs[1], dfs[4])
-    target_predicted = classifier.predict(dfs[2])
+        model = DecisionTreeClassifier(
+            max_depth=max_depth, 
+            min_samples_leaf=min_samples_leaf, 
+            min_samples_split=min_samples_split, 
+            max_features='log2',
+            random_state=int(random.random()*100)
+        )
 
-    return get_metrics(dfs[5, target_predicted])
+        score = cross_val_score(model, dfs[0], dfs[3], n_jobs=-1, cv=3).mean()
+        
+        return score
 
-def logistic_regression_classification(dataset_path: str, features: list, target: str) -> dict:
-    dfs = split_dataset(dataset_path, features, target)
+    study = optuna.create_study(direction='maximize', study_name='Decision_Tree')
+    study.optimize(objective, n_trials=100)
+    best_params = study.best_trial.params
 
-    classifier = LogisticRegression(max_iter=200, random_state=random.random()*100)
+    best_model = DecisionTreeClassifier(**best_params)
+    best_model.fit(dfs[1], dfs[4])
 
-    classifier.fit(dfs[1], dfs[4])
-    target_predicted = classifier.predict(dfs[2])
+    target_predicted = best_model.predict(dfs[2])
+    
+    return get_metrics(dfs[5], target_predicted)
 
-    return get_metrics(dfs[5, target_predicted])
+def logistic_regression_classification(dfs) -> dict:
+    def objective(trial):
+        
+        max_iter = trial.suggest_int('max_iter', 50, 100)
+        C = trial.suggest_float('C', 1, 2)
 
-def k_nearest_neighbors_classification(dataset_path: str, features: list, target: str) -> dict:
-    dfs = split_dataset(dataset_path, features, target)
+        model = LogisticRegression(
+            max_iter=max_iter, 
+            penalty='l2',
+            solver='lbfgs',
+            C=C,
+            random_state=int(random.random()*100)
+        )
 
-    classifier = KNeighborsClassifier(n_neighbors=5, algorithm='auto')
+        score = cross_val_score(model, dfs[0], dfs[3], n_jobs=-1, cv=3).mean()
+        
+        return score
 
-    classifier.fit(dfs[1], dfs[4])
-    target_predicted = classifier.predict(dfs[2])
+    study = optuna.create_study(direction='maximize', study_name='Logistic_Regression')
+    study.optimize(objective, n_trials=100)
+    best_params = study.best_trial.params
 
-    return get_metrics(dfs[5, target_predicted])
+    best_model = LogisticRegression(**best_params)
+    best_model.fit(dfs[1], dfs[4])
 
-def naive_bayes_classification(dataset_path: str, features: list, target: str) -> dict:
-    dfs = split_dataset(dataset_path, features, target)
+    target_predicted = best_model.predict(dfs[2])
 
-    classifier = GaussianNB()
+    return get_metrics(dfs[5], target_predicted)
 
-    classifier.fit(dfs[1], dfs[4])
-    target_predicted = classifier.predict(dfs[2])
+def k_nearest_neighbors_classification(dfs) -> dict:
+    def objective(trial):
+        
+        n_neighbors = trial.suggest_int('n_neighbors', 1, 20)
 
-    return get_metrics(dfs[5, target_predicted])
+        model = KNeighborsClassifier(
+            n_neighbors=n_neighbors,
+            algorithm='auto'
+        )
+
+        score = cross_val_score(model, dfs[0], dfs[3], n_jobs=-1, cv=3).mean()
+        
+        return score
+
+    study = optuna.create_study(direction='maximize', study_name="K_Nearest_Neighbors")
+    study.optimize(objective, n_trials=20)
+    best_params = study.best_trial.params
+
+    best_model = KNeighborsClassifier(**best_params)
+    best_model.fit(dfs[1], dfs[4])
+
+    target_predicted = best_model.predict(dfs[2])
+
+    return get_metrics(dfs[5], target_predicted)
+
+def naive_bayes_classification(dfs) -> dict:
+    def objective(trial):
+        
+
+        var_smoothing = trial.suggest_float('var_smoothing', 0.000000001, 0.0000001)
+
+        model = GaussianNB(
+            var_smoothing=var_smoothing
+        )
+
+        score = cross_val_score(model, dfs[0], dfs[3], n_jobs=-1, cv=3).mean()
+        
+        return score
+
+    study = optuna.create_study(direction='maximize', study_name='Naive_Bayes')
+    study.optimize(objective, n_trials=100)
+    best_params = study.best_trial.params
+
+    best_model = GaussianNB(**best_params)
+    best_model.fit(dfs[1], dfs[4])
+
+    target_predicted = best_model.predict(dfs[2])
+
+    return get_metrics(dfs[5], target_predicted)
--- bugs_tokenizer.py
+++ bugs_tokenizer.py
@@ -2,8 +2,12 @@ import nltk
 import os
 import shutil
 import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, LabelBinarizer
 
-string_features = [
+hot_features = [
+    "url",
     "classification",
     "component",
     "op_sys",
@@ -11,12 +15,38 @@ string_features = [
     "priority",
     "product",
     "resolution",
-    "severity",
     "status",
+    "is_open",
+]
+
+list_fetures = [
+    "flags",
+    "keywords",
+    "see_also",
+    "groups",
+]
+
+label_features = [
+    "priority",
+]
+
+textual_features = [
     "summary",
-    "url",
     "whiteboard",
 ]
+
+final_features = [
+    "depends_on",
+    "dupe_of",
+    "id",
+]
+
+target_feature = "severity"
+
+lemmatizer = nltk.stem.WordNetLemmatizer()
+vectorizer = TfidfVectorizer()
+
+NLP_data = []
 
 def download_nltk_files() -> None:
     for p in nltk.data.path:
@@ -27,16 +57,71 @@ def download_nltk_files() -> None:
     nltk.download('averaged_perceptron_tagger')
     nltk.download('maxent_ne_chunker')
     nltk.download('words')
+    nltk.download('stopwords')
+    nltk.download('wordnet')
 
 def get_features_tokens() -> None:
+    def tokenize(text: str):
+        if type(text) is not str:
+            return "empty"
+        tokens = nltk.tokenize.word_tokenize(text)
+        tokens =  [word for word in tokens if word.lower() not in stop_words]
+        tokens = [lemmatizer.lemmatize(word) for word in tokens]
+        return ' '.join(tokens)
+
+    def hot_encoder(data: list[str]):
+        data = [d if type(d) is str else "empty" for d in data]
+        he = LabelBinarizer()
+        return he.fit_transform(data)
+
+    def label_encoder(data: list[str]):
+        le = LabelEncoder()
+        return le.fit_transform(data)
+
+    try:
+        shutil.rmtree("tokenized")
+    except:
+        print(f"Path tokenized doesn't exist")
+    os.mkdir("tokenized")
+    
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    
     df = pd.read_csv("final_dataset.csv").copy()
 
-    for f in string_features:
+    for f in hot_features:
         print(f'INFO: Processing feature {f}')
-        df[f].apply(lambda data: nltk.tokenize.word_tokenize(str(data)))
+        aux = hot_encoder(df[f])
+        pd.DataFrame(aux).to_csv(f"tokenized/{f}.csv", index=False, header=[f'{f}-{i}' for i in range(len(aux[0]))])
 
-    df.to_csv("final_dataset_tokenazed.csv", index=False)
+    for f in textual_features:
+        print(f'INFO: Processing feature {f}')
+        df[f] = df[f].apply(tokenize)
+        aux = vectorizer.fit_transform(df[f]).toarray()
+        pd.DataFrame(aux).to_csv(f"tokenized/{f}.csv", index=False, header=[f'{f}-{i}' for i in range(len(aux[0]))])
+
+    for f in label_features:
+        print(f'INFO: Processing feature {f}')
+        aux = label_encoder(df[f])
+        pd.DataFrame(aux).to_csv(f"tokenized/{f}.csv", index=False, header=[f'{f}-0'])
+
+    print(f'INFO: Processing feature {target_feature}')
+    pd.DataFrame(label_encoder(df[target_feature])).to_csv(f"tokenized/{target_feature}.csv", index=False, header=['target'])
+        
     print("""
 
 INFO: Process finished!
+    """)
+
+def group_tokenzed_files():
+    files = os.listdir("tokenized")
+
+    data = []
+    for f in files:
+        print(f'INFO: Getting file {f}')
+        data.append(pd.read_csv(f'tokenized/{f}'))
+    df = pd.concat(data, axis=1)
+    df.to_csv("final_dataset_tokenazed.csv", index=False)
+    print("""
+
+INFO: File compiled!
     """)
